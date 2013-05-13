6a23281dd6186a2fd2750c23e774facb463f046a
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-13 17:38:09 +0200

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStreamStarted::Application.config.secret_token = '3ef448fa288b6f01fc6ada5c181dc1f9c753c30671d9b432c1afb523f9b4cddbde1fbe3473de5b594eab93ab8513575c3c4ace537a2a7cadcb19a4391e362297'
